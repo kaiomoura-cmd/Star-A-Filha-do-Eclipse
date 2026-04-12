f6b9ffc67d106e40763a229b661faa729ff8cfa6
Gustavo Simplício Bernardo <gustavosimpliciob@gmail.com>
2026-04-12 19:30:38 -0300

    Progresso mecânicas básicas
    Adicionei uma textura no placeholder do player para facilitar a identificação da direção em que o jogador está se movendo, melhorei o pulo com jump buffer e ajuste do controle aéreo e adicionei wall jump.

--- JogoCG/Assets/Scenes/Movement.cs
+++ JogoCG/Assets/Scenes/Movement.cs
@@ -13,22 +13,51 @@ public class Movement : MonoBehaviour
 
     public float jumpTime = 0.35f;
     private float jumpTimeCounter;
-    private bool isJumping;
-
     public float input;
 
+    public float sensibility = 0.7f;
+    
+    public float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
+    
+    private bool isWallSliding;
+    private float wallSlidingSpeed = 2f;
+
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.3f;
+    private Vector3 wallJumpingPower = new Vector3(8f, 8f);
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+
     private Rigidbody rb;
+    
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     void FixedUpdate()
     {
         float moveHorizontal = input;
-        rb.linearVelocity = new Vector3(moveHorizontal * speed, rb.linearVelocity.y);
+
+        float targetX = moveHorizontal * speed;
+
+        float airControl = isGrounded ? 0.8f : 0.05f;
+
+        float newX = Mathf.Lerp(rb.linearVelocity.x, targetX, airControl);
+
+        if (!isWallJumping)
+        {
+            rb.linearVelocity = new Vector3(newX, rb.linearVelocity.y);
+        }
         rb.linearVelocity += new Vector3(0, gravity * Time.fixedDeltaTime);
+
+        jumpBufferCounter -= Time.fixedDeltaTime;
 
         if (isGrounded)
         {
@@ -38,6 +67,22 @@ public class Movement : MonoBehaviour
         {
             coyoteTimeCounter -= Time.fixedDeltaTime;
         }
+
+        WallSlide();
+        WallJump();
+
+        if (!isWallJumping)
+        {
+            Flip();
+        }
+
+        JumpHandler();
+    }
+
+    public void Flip()
+    {
+        if (input > 0) transform.localScale = new Vector3(1, 2, 1);
+        else if (input < 0) transform.localScale = new Vector3(-1, 2, 1);
     }
 
     public void OnMove(InputValue value)
@@ -47,13 +92,73 @@ public class Movement : MonoBehaviour
 
     public void OnJump()
     {
+        jumpBufferCounter = jumpBufferTime;
+        if(wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.linearVelocity = new Vector3(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if(transform.localScale.x != wallJumpingDirection)
+            {
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
+    public void JumpHandler()
+    {
+        if (jumpBufferCounter <= 0f) return;
+
         if (coyoteTimeCounter > 0f)
         {
             rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce);
             isGrounded = false;
             coyoteTimeCounter = 0f;
-            isJumping = true;
             jumpTimeCounter = jumpTime;
+        }
+
+    }
+
+    private bool IsWalled()
+    {
+        return Physics.CheckSphere(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private void WallSlide()
+    {
+        if(IsWalled() && !isGrounded && input != 0f)
+        {
+            isWallSliding = true;
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, Mathf.Clamp(rb.linearVelocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    private void WallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
         }
     }
 
@@ -61,7 +166,29 @@ public class Movement : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = true;
+            foreach(ContactPoint contact in collision.contacts)
+            {
+                if(contact.normal.y > sensibility)
+                {
+                    isGrounded = true;
+                    return;
+                }
+            }
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            foreach (ContactPoint contact in collision.contacts)
+            {
+                if (contact.normal.y > sensibility)
+                {
+                    isGrounded = true;
+                    return;
+                }
+            }
         }
     }
 
